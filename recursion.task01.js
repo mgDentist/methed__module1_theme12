e5ee399a4cff5d00d85a72f428241ca07ed467e3
mgDentist <gmi1987@gmail.com>
2024-02-27 19:38:28 +0400

    module1-theme12-task01-recursion

---
+++ recursion.task01.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const numberBotGame = (botNumber = Math.floor((Math.random()) * 100) + 1) => {
+  let userAnswer = prompt('Какое число от 1 до 100 я загадал?');
+
+  if (userAnswer === null) {
+    alert('Игра закончена');
+    return;
+  }
+
+  userAnswer = Number(userAnswer);
+
+  if (isNaN(userAnswer)) {
+    alert('Введи число!');
+    numberBotGame(botNumber);
+    return;
+  }
+
+  if (botNumber === userAnswer) {
+    alert('Правильно');
+  } else if (userAnswer < botNumber) {
+    alert('Больше');
+  } else if (userAnswer > botNumber) {
+    alert('Меньше');
+  }
+
+  numberBotGame(botNumber);
+};
+
+
+numberBotGame();
